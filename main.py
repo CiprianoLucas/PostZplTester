dfa3bb9a08b82268ab260343c94729370ae8ef56
Lucas Cipriano <lucas.cipriano@datafrete.com.br>
2025-04-10 14:58:17 -0300

    feat: aceita outros arquivos agora

--- main.py
+++ main.py
@@ -2,31 +2,34 @@ from fastapi import FastAPI
 from pydantic import BaseModel
 import base64
 import os
+from io import BytesIO
 
 app = FastAPI()
 
 class SendStringRequest(BaseModel):
-    zpl: str
-    codigoRastreamento: str
+    content: str
+    tracking: str
+    responseType: str
+    fileType: str
 
 @app.post("/send_string/")
 async def send_string(request: SendStringRequest):
-    
-    string = request.zpl
-    tracking_code = request.codigoRastreamento
+    file = request.content
+    tracking_code = request.tracking
+    response_type = request.responseType
+    file_type = request.fileType
 
-    base64_bytes = string.encode('utf-8')
-    string_bytes = base64.b64decode(base64_bytes)
-    decoded_string = string_bytes.decode('utf-8')
-    
+    file_bytes = file.encode()
+    if response_type == 'base64':
+        file_bytes = base64.b64decode(file_bytes)
+
     directory = os.path.join(os.path.dirname(__file__), 'ZPLs')
-    
-    if not os.path.exists(directory):
-        os.makedirs(directory)
-        
-    file_path = os.path.join(directory, f"{tracking_code}.txt")
-            
-    with open(file_path, "w") as file:
-        file.write(decoded_string)
 
-    return {"message": "Arquivo criado com sucesso"}
+    os.makedirs(directory, exist_ok=True)
+
+    file_path = os.path.join(directory, f"{tracking_code}.{file_type}")
+
+    with open(file_path, "wb") as f:
+        f.write(file_bytes)
+
+    return {"message": "Arquivo salvo com sucesso", "file_path": file_path}
